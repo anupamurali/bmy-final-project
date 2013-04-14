0112417c3d2bcb59ed5072c1dd604400696f3aff
Zach Young <zyoung@college.harvard.edu>
2013-04-14 15:16:50 -0400

    Point and mainclass!

---
+++ src/Point.java
@@ -0,0 +1,17 @@
+
+public class Point {
+
+	double x;
+	double y;
+	public Point(double xh, double yh)
+	{
+		x = xh;
+		y = yh;
+	}
+	public void setx (double xh) {x = xh;}
+	public void sety (double yh) {y = yh;}
+	public double getx () {return x;}
+	public double gety () {return y;}
+	
+	
+}
