acc4be1130c73cd9934be7ae0f2cca90af91fc04
Meena Boppana <boppana@college.harvard.edu>
2013-04-20 18:01:44 -0400

    Commiting

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -4,9 +4,28 @@ public class Mainclass {
 	/**
 	 * @param args
 	 */
+	
+	sites = ArrayList<Site>;
+	//beachline = ArrayList<ParabolaSegment>;	
+	
+	siteEvents = ArrayList<Hline>;
+		//Keeps the list of site events ordered as a priority queue.
+    //circleEvents = ArrayList<Hline>;
+	//Keeps the list of circle events ordered as a priority queue.
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+	
+	}			
+	
+    public ArrayList<double> genSiteEvents(){
+                 
+    }
+    
+    public ArrayList<double> genCircleEvents(){
+    	
+    }
+    
 
 }
