efaa298ce31571ca731d4a478b1ffa5a22baaca3
Meena Boppana <boppana@college.harvard.edu>
2013-04-20 18:07:35 -0400

    Commit

--- src/Parabola.java
+++ src/Parabola.java
@@ -1,3 +1,5 @@
+
+
 public class Parabola
 {
   Point focus;
@@ -8,28 +10,18 @@ public class Parabola
          directrix = d;
   }
  
-    public point getFocus (Parabola i)
-    {
-      
-    }
+    public point getFocus () {return f;}
+ 
+    public hline getDirectrix () {return d;}
 
 //Anupa
   public point[] intersectParabola (Parabola i)
-  {
-	//return an array of the intersection points of i with this, or null
-       
+  {       
     
   }
 
-equation of a  parabola  is (x-h)^2 = 4p(y-k)  where the focus is (h,k+p) and directrix is y=k-p.    (http://jwilson.coe.uga.edu/emt725/class/sarfaty/emt669/instructionalunit/parabolas/parabolas.html)
+    //equation of a  parabola  is (x-h)^2 = 4p(y-k)  where the focus is (h,k+p) and directrix is y=k-p.    (http://jwilson.coe.uga.edu/emt725/class/sarfaty/emt669/instructionalunit/parabolas/parabolas.html)
 
 }
-//Anupa, make get and set for ParabolaSegment. And other stuff in Parabola.
-public class ParabolaSegment
-{
-float start;
-float end;
-Parabola p;
-//usual constructors, get and set functions
-}
+
 
