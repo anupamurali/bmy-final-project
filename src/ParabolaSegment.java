81f90e7089332ad5ecac03aa9f578832b8a726a3
Meena Boppana <boppana@college.harvard.edu>
2013-04-20 18:08:14 -0400

    here it is

---
+++ src/ParabolaSegment.java
@@ -0,0 +1,8 @@
+//Anupa, make get and set for ParabolaSegment. And other stuff in Parabola.
+public class ParabolaSegment
+{
+float start;
+float end;
+Parabola p;
+//usual constructors, get and set functions
+}
