76a1198d07ac9909442edf5a61e8560a50122c1b
Anupa Murali <anupamurali@college.harvard.edu>
2013-04-20 22:07:02 -0400

    HERE IT IS

--- src/Parabola.java
+++ src/Parabola.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class Parabola
 {
@@ -19,13 +19,49 @@ public class Parabola
     public void setDirectrix (Hline newDirectrix) {directrix = newDirectrix;}
 
 //Anupa 
-  public Point[] intersectParabola (Parabola i)
+  public ArrayList <Point> intersectParabola (Parabola i)
   {       
       Point focus1 = this.getFocus ();
       Point focus2 = i.getFocus ();
       Hline directrix1 = this.getDirectrix ();
       Hline directrix2 = i.getDirectrix ();     
 
-      return [];
+      double a = focus1.getx ();
+      double b = focus1.gety ();
+      double c = directrix1.gety ();
+      double d = focus2.getx ();
+      double e = focus2.gety ();
+      double f = directrix2.gety ();
+
+      double p = 1/(b-c) - 1/(e-f);
+      double q = 2*d/(e-f) - 2*a/(b-c);
+      double r = (a*a+b*b-c*c)/(b-c) - (d*d + e*e - f*f)/(e-f);
+
+      double discriminant = q*q-4*p*r;
+
+      ArrayList<Point> soltn = new ArrayList<Point>();       
+    
+
+      if (p == 0) 
+	  {
+	      if (q!=0)     
+                  {soltn.add(new Point ((-r/q), (((-r/q)-a)*((-r/q)-a) + b*b - c*c)/(2*(b-c))));}
+	      return soltn;
+
+	  }
+      if (discriminant > 0)
+      {
+       double x1 = (-q-Math.sqrt(discriminant))/(2*p);
+       double x2 = (-q+Math.sqrt(discriminant))/(2*p);
+       soltn.add(new Point (x1, ((x1-a)*(x1-a) + b*b - c*c)/(2*(b-c))));
+       soltn.add(new Point (x2, ((x2-a)*(x2-a) + b*b - c*c)/(2*(b-c))));
+       }
+      else if (discriminant == 0)
+      { 
+	 double x = -q/(2*p);
+         soltn.add(new Point (x,((x-a)*(x-a) + b*b - c*c)/(2*(b-c))));
+      }
+      else throw new ArithmeticException ();
+      return soltn;
   }
 }
--- src/ParabolaSegment.java
+++ src/ParabolaSegment.java
@@ -1,8 +1,28 @@
 //Anupa, make get and set for ParabolaSegment. And other stuff in Parabola.
 public class ParabolaSegment
 {
-float start;
-float end;
-Parabola p;
+  double start;
+  double end;
+  Parabola p;
 //usual constructors, get and set functions
+
+  public ParabolaSegment (double s, double e, Parabola para)
+  {
+         start = s;
+         end = e;
+         p = para;
+  }
+
+    public double getStart () {return start;}
+    
+    public double getEnd () {return end;}
+ 
+    public Parabola getParabola () {return p;}
+
+    public void setStart (double new_start) {start = new_start;}
+
+    public void setEnd (double new_end) {end = new_end;}
+
+    public void setParabola (Parabola new_p) {p = new_p;}
 }
+
